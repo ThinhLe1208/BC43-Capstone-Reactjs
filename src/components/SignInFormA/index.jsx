73d8a993fec96c4955f79b06163bc414502d2f4b
AAnn1912 <phamthienan862@gmail.com>
2023-06-10 01:29:34 +0700

    finish

--- src/components/SignInFormA/index.jsx
+++ src/components/SignInFormA/index.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-import styles from './styles.module.scss';
-import InputField from 'components/InputFieldA';
-import { Button, Space, Spin } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
-import { usersThunk } from 'redux/thunks/usersThunk';
-import { storage } from 'utils/storage';
-import { ACCESS_TOKEN, USER_LOGIN } from 'utils/constants/settingSystem';
-import { notifications } from 'utils/notifications';
-import { history } from 'utils/history';
-import SocialButton from 'components/SocialButton';
+import styles from "./styles.module.scss";
+import InputField from "components/InputFieldA";
+import { Button, Space, Spin } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { usersThunk } from "redux/thunks/usersThunk";
+import { storage } from "utils/storage";
+import { ACCESS_TOKEN, USER_LOGIN } from "utils/constants/settingSystem";
+import { notifications } from "utils/notifications";
+import { history } from "utils/history";
+import SocialButton from "components/SocialButton";
 
 const SignInSchema = Yup.object().shape({
-  email: Yup.string().required('Please provide email.').email('Email is invalid'),
+  email: Yup.string()
+    .required("Please provide email.")
+    .email("Email is invalid"),
   password: Yup.string()
-    .required('Please provide password.')
-    .matches('^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$', 'Password is invalid'),
+    .required("Please provide password.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    ),
 });
 
 const SignInForm = () => {
@@ -25,100 +30,92 @@ const SignInForm = () => {
   const dispatch = useDispatch();
 
   // Formik
-  const { values, errors, touched, handleChange, handleBlur, handleSubmit } = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema: SignInSchema,
-    onSubmit: async (values) => {
-      try {
-        const response = await dispatch(usersThunk.signIn(values)).unwrap();
-        storage.setStorageJson(USER_LOGIN, response);
-        storage.setStorageJson(ACCESS_TOKEN, response.accessToken);
-        storage.setCookieJson(USER_LOGIN, response, 30);
-        notifications.success('Sign in successfully.');
-        history.push('/index');
-      } catch (err) {
-        notifications.error('Failed to sign in.');
-      }
-    },
-  });
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: SignInSchema,
+      onSubmit: async (values) => {
+        try {
+          const response = await dispatch(usersThunk.signIn(values)).unwrap();
+          storage.setStorageJson(USER_LOGIN, response);
+          storage.setStorageJson(ACCESS_TOKEN, response.accessToken);
+          storage.setCookieJson(USER_LOGIN, response, 30);
+          notifications.success("Sign in successfully.");
+          history.push("/index");
+        } catch (err) {
+          notifications.error("Failed to sign in.");
+        }
+      },
+    });
 
   const handleSocialLogin = async (user) => {
     try {
-      const response = await dispatch(usersThunk.facebooklogin(user?._token?.accessToken)).unwrap();
+      const response = await dispatch(
+        usersThunk.facebooklogin(user?._token?.accessToken)
+      ).unwrap();
       storage.setStorageJson(USER_LOGIN, response);
       storage.setStorageJson(ACCESS_TOKEN, response.accessToken);
       storage.setCookieJson(USER_LOGIN, response, 30);
-      notifications.success('Sign in with Facebook successfully.');
-      history.push('/index');
+      notifications.success("Sign in with Facebook successfully.");
+      history.push("/index");
     } catch (err) {
-      notifications.error('Failed to sign in with Facebook.');
+      notifications.error("Failed to sign in with Facebook.");
     }
   };
 
   const handleSocialLoginFailure = (err) => {
     if (err) {
-      notifications.error('Failed to sign in with Facebook.');
+      notifications.error("Failed to sign in with Facebook.");
     }
   };
 
   return (
-    <div
-      className={styles.wrapper}
-      style={{ paddingTop: '20px' }}
-    >
+    <div className={styles.wrapper} style={{ paddingTop: "20px" }}>
       <form onSubmit={handleSubmit}>
         <div className={styles.input}>
           <InputField
-            label='Email'
-            name='email'
+            label="Email"
+            name="email"
             value={values.email}
             error={errors.email}
             touched={touched.email}
-            placeholder=' '
+            placeholder=" "
             onChange={handleChange}
             onBlur={handleBlur}
           />
         </div>
         <div className={styles.input}>
           <InputField
-            label='Password'
-            name='password'
+            label="Password"
+            name="password"
+            type="password"
             value={values.password}
             error={errors.password}
             touched={touched.password}
-            placeholder=' '
+            placeholder=" "
             onChange={handleChange}
             onBlur={handleBlur}
           />
         </div>
-        <div
-          className={styles.flex}
-          style={{ paddingTop: '20px' }}
-        >
+        <div className={styles.flex} style={{ paddingTop: "20px" }}>
           <Space>
-            <Button
-              type='primary'
-              htmlType='submit'
-            >
+            <Button type="primary" htmlType="submit">
               Sign In
             </Button>
             <SocialButton
-              provider='facebook'
-              appId='259894119932234'
+              provider="facebook"
+              appId="259894119932234"
               disabled={isLoadingUsers}
-              type='primary'
+              type="primary"
               onLoginSuccess={handleSocialLogin}
               onLoginFailure={handleSocialLoginFailure}
             >
               <Space>
                 Sign in with Facebook
-                <Spin
-                  spinning={isLoadingUsers}
-                  style={{ color: '#fff' }}
-                />
+                <Spin spinning={isLoadingUsers} style={{ color: "#fff" }} />
               </Space>
             </SocialButton>
           </Space>
--- src/components/SignUpFormA/index.jsx
+++ src/components/SignUpFormA/index.jsx
@@ -1,50 +1,63 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-import styles from './styles.module.scss';
-import InputField from 'components/InputFieldA';
-import { Button, Radio } from 'antd';
-import { useDispatch } from 'react-redux';
-import { usersThunk } from 'redux/thunks/usersThunk';
-import { notifications } from 'utils/notifications';
-import { history } from 'utils/history';
+import styles from "./styles.module.scss";
+import InputField from "components/InputFieldA";
+import { Button, Radio } from "antd";
+import { useDispatch } from "react-redux";
+import { usersThunk } from "redux/thunks/usersThunk";
+import { notifications } from "utils/notifications";
+import { history } from "utils/history";
 
 const SignUpSchema = Yup.object().shape({
-  email: Yup.string().required('Please provide email.').email('Email is invalid'),
+  email: Yup.string()
+    .required("Please provide email.")
+    .email("Email is invalid"),
   password: Yup.string()
-    .required('Please provide password.')
-    .matches('^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$', 'Password is invalid'),
-  name: Yup.string().required('Please provide name.').min(6).max(10),
-  phone: Yup.string().required('Please provide phone.').max(10),
+    .required("Please provide password.")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+    ),
+  name: Yup.string().required("Please provide name.").min(6).max(24),
+  phone: Yup.string().required("Please provide phone.").max(10),
 });
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
 
   // Formik
-  const { values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue } = useFormik({
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    setFieldValue,
+  } = useFormik({
     initialValues: {
-      email: '',
-      password: '',
-      name: '',
+      email: "",
+      password: "",
+      name: "",
       gender: true,
-      phone: '',
+      phone: "",
     },
     validationSchema: SignUpSchema,
     onSubmit: async (values) => {
       try {
         await dispatch(usersThunk.signUp(values)).unwrap();
-        notifications.success('Sign up successfully.');
-        history.push('/login');
+        notifications.success("Sign up successfully.");
+        history.push("/login");
       } catch (err) {
-        notifications.error('Failed to sign up.');
+        notifications.error("Failed to sign up.");
       }
     },
   });
 
   const handleChangeGender = (e) => {
-    setFieldValue('gender', e.target.value);
+    setFieldValue("gender", e.target.value);
   };
 
   return (
@@ -52,65 +65,60 @@ const SignUpForm = () => {
       <form onSubmit={handleSubmit}>
         <div className={styles.input}>
           <InputField
-            label='Email'
-            name='email'
+            label="Email"
+            name="email"
             value={values.email}
             error={errors.email}
             touched={touched.email}
-            placeholder=' '
+            placeholder=" "
             onChange={handleChange}
             onBlur={handleBlur}
           />
         </div>
         <div className={styles.input}>
           <InputField
-            label='Password'
-            name='password'
+            label="Password"
+            name="password"
+            type="password"
             value={values.password}
             error={errors.password}
             touched={touched.password}
-            placeholder=' '
+            placeholder=" "
             onChange={handleChange}
             onBlur={handleBlur}
           />
         </div>
         <div className={styles.input}>
           <InputField
-            label='Name'
-            name='name'
+            label="Name"
+            name="name"
             value={values.name}
             error={errors.name}
             touched={touched.name}
-            placeholder=' '
+            placeholder=" "
             onChange={handleChange}
             onBlur={handleBlur}
           />
         </div>
         <div className={styles.input}>
           <InputField
-            label='Phone'
-            name='phone'
+            label="Phone"
+            name="phone"
             value={values.phone}
             error={errors.phone}
             touched={touched.phone}
-            placeholder=' '
+            placeholder=" "
             onChange={handleChange}
             onBlur={handleBlur}
           />
         </div>
         <div className={styles.input}>
-          <Radio.Group
-            onChange={handleChangeGender}
-            value={values.gender}
-          >
+          <Radio.Group onChange={handleChangeGender} value={values.gender}>
             <Radio value={true}>Male</Radio>
             <Radio value={false}>Female</Radio>
           </Radio.Group>
         </div>
-        <Button
-          type='primary'
-          htmlType='submit'
-        >
+        <Button type="primary" htmlType="submit">
           Sign Up
         </Button>
       </form>
